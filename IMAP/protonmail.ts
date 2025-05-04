1057d04c8c9440d05b066f39d1e1c4fc52ecb31a
U82146510 <U82146510@protonmail.com>
2025-05-04 16:02:01 +0100

    new updates

---
+++ IMAP/protonmail.ts
@@ -0,0 +1,21 @@
+import dotnev from 'dotenv';
+dotnev.config();
+import {ImapFlow} from 'imapflow'
+
+
+const user = process.env.user;
+const pass = process.env.pass;
+if(!user||!pass){
+    throw new Error("missing user or pass");
+};
+
+
+export const client = new ImapFlow({
+    host:"imap.gmail.com",
+    port:993,
+    secure:true,
+    auth:{
+        user:user,
+        pass:pass
+    }
+});
--- middleware/email.ts
+++ middleware/email.ts
@@ -1,8 +1,36 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import {client} from '../IMAP/protonmail.ts';
+import {simpleParser} from 'mailparser';
 
-const api = process.env.api_sim;
-if(!api){
-    console.error('missing api');
-    process.exit(1);
-};
+async function read_mail() {
+    await client.connect();
+    let imbox_lock;
+    try {
+        
+        imbox_lock = await client.getMailboxLock('INBOX');
+        const messages = client.fetch("1:*",{
+            envelope:true,
+            source:true,
+            uid:true
+        });
+
+        for await (let msg of messages){
+            if(msg.source){
+                const parsed = await simpleParser(msg.source);
+                console.log('From:', parsed.from?.text);
+                console.log('Subject:', parsed.subject);
+                console.log('Text:', parsed.text?.slice(0, 200));
+                console.log('---');
+            }
+        }
+    } catch (error) {
+        console.log(error);
+    } finally{
+        if(imbox_lock){
+            imbox_lock.release();
+            await client.logout();
+        }
+        
+    }
+}
+
+read_mail();
