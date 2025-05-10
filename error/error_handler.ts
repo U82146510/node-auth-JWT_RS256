c4d0b0b6054ad62bf7c651a2e6dc09087ad34414
zak16 <zak@myrecovery.ai>
2025-05-10 21:15:41 +0100

    changed someotijea

--- error/error_handler.ts
+++ error/error_handler.ts
@@ -1,9 +1,9 @@
 import { type Request,type Response,type NextFunction } from "express"
 
-export const error_handler = (err:unknown,req:Request,res:Response,next:NextFunction)=>{
+export const error_handler_max = (err:unknown,req:Request,res:Response,next:NextFunction)=>{
     if(err instanceof Error){
         res.status(500).json({message:err.message});
         return;
     }
-    res.status(500).json({message:"Internal server error"});
+    res.status(500).json({message:"Internal server error again again again"});
 }
